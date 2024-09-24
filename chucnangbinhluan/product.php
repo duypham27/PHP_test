5c8e28984bb3572f9bd1a5fa87fa19837a5d1b40
Duy7ham <phambachanduy@gmail.com>
2024-09-24 10:05:41 +0700

    Sửa tiếp

--- chucnangbinhluan/product.php
+++ chucnangbinhluan/product.php
@@ -4,7 +4,7 @@
     $sql = "SELECT * FROM product";
     $result = mysqli_query($conn, $sql);
     while($row = mysqli_fetch_array($result))
-    { ?>
+    {
     <br>
     <?php echo $row['name_prd']; ?>
 
